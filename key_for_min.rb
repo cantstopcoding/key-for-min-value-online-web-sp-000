2d6822fc106d7b765e33a605f43721920c6c2421
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-07 18:43:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   low_num = 0
   
   name_hash.each do |name, num|
-    if low_num == <= 0 || num < low_num
+    if low_num <= 0 || num < low_num
       low_num_name = name 
       low_num = num 
     end 
